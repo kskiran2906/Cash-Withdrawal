536d127907520b1bd94baec1d6e88614494049a6
kskiran2906 <ksaikiran2906@gmail.com>
2021-08-29 12:59:16 +0000

    Cash Withdrawal

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,28 @@
+import './index.css'
+import {Component} from 'react'
+
+class DenominationItem extends Component {
+  onDeleteAmount = () => {
+    const {denomination, deleteAmount} = this.props
+    const {value} = denomination
+    deleteAmount(value)
+  }
+
+  render() {
+    const {denomination} = this.props
+    const {value} = denomination
+    return (
+      <li className="list-item">
+        <button
+          type="button"
+          className="denomination value"
+          onClick={this.onDeleteAmount}
+        >
+          {value}
+        </button>
+      </li>
+    )
+  }
+}
+
+export default DenominationItem
